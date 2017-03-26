0b25a87cb957c2d00bca6dfb156f043f7f86c76c
Levi Donaldson <lcdbacanas@gmail.com>
2017-03-25 23:44:47 -0500

    added first pure function

--- fp.js
+++ fp.js
@@ -1,2 +1,14 @@
 // This will be the functional programming concepts I learn 
 // in javascript. 
+
+// *** The first practice lesson is on pure functions. *** 
+// This is a pure function because the result takes no variables
+// or arguments from outside the function
+
+const PI = 3.14;
+const calculateArea = (radius, PI) => radius * radius * PI;
+console.log(calculateArea(3, 1));
+
+    // This should log 9 in part because we're 
+    // only logging whole numbers.
+
