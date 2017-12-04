762423fca3915bd47e27f6664096fc4433e82d06
Levi Donaldson <lcdbacanas@gmail.com>
2017-12-03 19:24:19 -0600

    React click events with jsx

---
+++ app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Creating a function to change images on click event of original image and 
+// setting jsx elements to variables for original image
+
+function makeBlue(e) {
+  e.target.setAttribute('src', 'http://www.clipartmasters.com/clip-arts/29/blue-square-clip-art-29530.png');
+  e.target.setAttribute('alt', 'blue');
+}
+
+const orange = (
+	<img onClick = {makeBlue}
+		src="http://www.clker.com/cliparts/2/R/v/e/o/L/orange-square-md.png" 
+		alt="orange" />
+);
+
+ReactDOM.render(orange, document.getElementById('app'));
