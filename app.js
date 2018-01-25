08733f14f2f334f469b38936a55670f8bd197a48
Levi Donaldson <lcdbacanas@gmail.com>
2018-01-24 19:08:12 -0600

    Update app.js

--- app.js
+++ app.js
@@ -10,10 +10,13 @@ function makeBlue(e) {
 }
 
 function makeRed(e) {
-  e.target.setAttribute('src', 'http://www.clipartmasters.com/clip-arts/29/red-square-clip-art-29530.png');
+  e.target.setAttribute('src', 'http://www.clipartmasters.com/clip-arts/29/red-square-clip-art-29531.png');
   e.target.setAttribute('alt', 'red');
 }
-
+function makeYellow(e) {
+  e.target.setAttribute('src', 'http://www.clipartmasters.com/clip-arts/29/yellow-square-clip-art-29532.png');
+  e.target.setAttribute('alt', 'yellow');
+}
 const orange = ( <img onClick = {makeBlue} src="http://www.clker.com/cliparts/2/R/v/e/o/L/orange-square-md.png" 
 	alt="orange" />
 );
