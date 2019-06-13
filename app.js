d426036ff504b6c62b540421983ecdcd58694409
Levi Donaldson <lcdbacanas@gmail.com>
2019-06-13 12:12:25 -0500

    Update app.js

--- app.js
+++ app.js
@@ -13,22 +13,23 @@ function makeRed(e) {
   e.target.setAttribute('src', 'http://www.clipartmasters.com/clip-arts/29/red-square-clip-art-29531.png');
   e.target.setAttribute('alt', 'red');
 }
+
 function makeYellow(e) {
   e.target.setAttribute('src', 'http://www.clipartmasters.com/clip-arts/29/yellow-square-clip-art-29532.png');
   e.target.setAttribute('alt', 'yellow');
 }
 
-const orange = Component => class extends React.Component {
+const colorBlock = Component => class extends React.Component {
     render() {
         return (
-		<div>
-			<p>I am glad you could see this in action</p>
-			<img onClick = {makeBlue} src="http://www.clker.com/cliparts/2/R/v/e/o/L/orange-square-md.png" 
-			alt="orange" />
-		</div>
-	);
+			<div>
+				<p>I am glad you could see this in action</p>
+				<img onClick = { makeBlue } src="http://www.clker.com/cliparts/2/R/v/e/o/L/orange-square-md.png" 
+				alt="orange" />
+			</div>
+		);
     }
 };
 export default Static;
 
-ReactDOM.render(orange, document.getElementById('app'));
+ReactDOM.render(colorBlock, document.getElementById('app'));
