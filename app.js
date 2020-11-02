85b84b11d1d625b2272053f03420775818e25754
Levi Donaldson <lcdbacanas@gmail.com>
2020-11-01 19:41:48 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Creating a function to change images on click event of original image and 
-// setting jsx elements to variables for original image
-
 function makeBlue(e) {
   e.target.setAttribute('src', 'http://www.clipartmasters.com/clip-arts/29/blue-square-clip-art-29530.png');
   e.target.setAttribute('alt', 'blue');
